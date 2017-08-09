614be93636f2cd681b7f92da41879106b64a9cca
yaozh16 <30818954+yaozh16@users.noreply.github.com>
2017-08-09 09:06:07 +0800

    Update MyMapWriter.h

--- MyMapWriter/MyMapWriter.h
+++ MyMapWriter/MyMapWriter.h
@@ -9,24 +9,28 @@
 #include <iomanip>
 #include <vector>
 using namespace std;
-typedef char maptype;
+typedef unsigned short maptype;
 typedef struct
 {
     int x;
     int y;
-}Point;
+} Point;
 class MyMapWriter
 {
 
 public:
     enum outputType {BMP};
-    MyMapWriter(std::string fileName,int width,int height,maptype* mapData,int channel=1,outputType type=BMP,std::string fileDirectory=""){
+    MyMapWriter(std::string fileName,int width,int height,
+                maptype* mapData,int channel=1,outputType type=BMP,
+                std::string fileDirectory=""){
         Set(fileName,width,height,mapData,channel,type,fileDirectory);
         Trajectory.clear();
     }
-    int Set(std::string fileName,int width,int height,maptype* mapData,int channel=1,outputType type=BMP,std::string fileDirectory="");
+    int Set(std::string fileName,int width,int height,
+            maptype* mapData,int channel=1,outputType type=BMP,
+            std::string fileDirectory="");
     int Write();
-    int AddMarker(vector<Point> _Trajectory, RGBQUAD MarkerColor);
+    int AddMarker(vector<Point> _Trajectory, RGBQUAD MarkerColor , int MarkerWidth=2);
 private:
     std::string fileName;
     std::string fileDirectory;
@@ -35,6 +39,7 @@ private:
     maptype* mapData;
     vector<Point> Trajectory;
     RGBQUAD MarkerColor;
+    int MarkerWidth;
     outputType type;
     int step;
     int channel;
@@ -49,19 +54,23 @@ int MyMapWriter::Set(string _fileName,int _width,int _height,maptype *_mapData,i
     channel=_channel,
     type=_type,
     fileDirectory=_fileDirectory;
+    Trajectory.clear();
         return 0;
 }
 
-int MyMapWriter::AddMarker(vector<Point> _Trajectory,RGBQUAD _MarkerColor)
+int MyMapWriter::AddMarker(vector<Point> _Trajectory,RGBQUAD _MarkerColor, int _MarkerWidth)
 {
     Trajectory=_Trajectory;
     MarkerColor=_MarkerColor;
+    MarkerWidth=_MarkerWidth;
+    channel=3;
     return 0;
 }
 
 int MyMapWriter::Write()
 {
-    FILE *pFile;
+    ofstream pf;
+    streampos pixelDataOffset;
     BITMAPFILEHEADER bmpFileHeader;
     BITMAPINFOHEADER bmpInfoHeader;
     int step;
@@ -70,10 +79,7 @@ int MyMapWriter::Write()
     int i, j;
     RGBQUAD* quad;
     std::string path=fileDirectory+fileName;
-    pFile = fopen(path.c_str(), "wb");
-    if (!pFile)
-        return 1;
-
+    pf.open(path.c_str(),ios_base::binary|ios_base::out);
     bmpFileHeader.bfType=0x4D42;
 
     if (channel == 3)//24位，通道，彩图
@@ -81,7 +87,7 @@ int MyMapWriter::Write()
         cout<<"output from 24bit map...";
         step =channel*width;
         offset = step%4;
-        if (offset != 4)
+        if (offset != 0)
         {
             step += 4-offset;
         }
@@ -90,7 +96,7 @@ int MyMapWriter::Write()
         bmpFileHeader.bfReserved1 = 0;
         bmpFileHeader.bfReserved2 = 0;
         bmpFileHeader.bfOffBits = 54;
-        fwrite(&bmpFileHeader, sizeof(BITMAPFILEHEADER), 1, pFile);
+        pf.write((const char*)&bmpFileHeader,sizeof(BITMAPFILEHEADER));
 
         bmpInfoHeader.biSize = 40;
         bmpInfoHeader.biWidth = width;
@@ -103,33 +109,63 @@ int MyMapWriter::Write()
         bmpInfoHeader.biYPelsPerMeter = 0;
         bmpInfoHeader.biClrUsed = 0;
         bmpInfoHeader.biClrImportant = 0;
-        fwrite(&bmpInfoHeader, sizeof(BITMAPINFOHEADER), 1, pFile);
+        pf.write((const char*)&bmpInfoHeader, sizeof(BITMAPINFOHEADER));
+
+        pixelDataOffset=pf.tellp();
+
         cout<<"setting pixels..."<<endl;
         cout<<"output "<<fileName<<"..."<<std::setw(4)<<100<<"% left...";
         for(int tem=0;tem<13;tem++)
             cout<<"\b";
+        /////////////////////
+        //存储方式：对于图像
+        //123
+        //456   转化为789_456_123
+        //789
+        //      (x,y)在(x*channel+(height-y)*step)
+        ///////////////////
         for (i=height-1; i>-1; i--)
         {
             cout<<std::setw(4)<<int(double(i+1)/double(height)*100)<<"% left...";
             for (j=0; j<width; j++)
             {
                 pixVal = mapData[i* width+j];
-                fwrite(&pixVal, sizeof(unsigned char), 1, pFile);
-                fwrite(&pixVal, sizeof(unsigned char), 1, pFile);
-                fwrite(&pixVal, sizeof(unsigned char), 1, pFile);
+                pf.write((const char*)&pixVal, sizeof(unsigned char));
+                pf.write((const char*)&pixVal, sizeof(unsigned char));
+                pf.write((const char*)&pixVal, sizeof(unsigned char));
             }
             if (offset!=0)
             {
                 for (j=0; j<offset; j++)
                 {
                     pixVal = 0;
-                    fwrite(&pixVal, sizeof(unsigned char), 1, pFile);
+                    pf.write((const char*)&pixVal, sizeof(unsigned char));
                 }
             }
             for(int tem=0;tem<13;tem++)
                 cout<<"\b";
         }
-        cout<<"Done."<<endl;
+        cout<<"Done         "<<endl;
+        if(!Trajectory.empty())
+        {
+            unsigned char Marker[3]={MarkerColor.rgbBlue,MarkerColor.rgbGreen,MarkerColor.rgbRed};
+            for(auto iter:Trajectory)
+            {
+                for(int px=iter.x-MarkerWidth+1;px<iter.x+MarkerWidth;px++)
+                    for(int py=iter.y-MarkerWidth+1;py<iter.y+MarkerWidth;py++)
+                        if(px>=0&&px<width&&py>=0&&py<height)
+                        {
+                            int dx=px-iter.x;
+                            int dy=py-iter.y;
+                            if(dx*dx+dy*dy<=MarkerWidth*MarkerWidth)
+                            {
+                                pf.seekp(pixelDataOffset);
+                                pf.seekp((px*channel+(height-py)*(step)),ios_base::cur);
+                                pf.write((const char*)Marker,3*(sizeof(unsigned char)));
+                            }
+                        }
+            }
+        }
     }
     else if (channel == 1)//8位，单通道，灰度图
     {
@@ -145,7 +181,7 @@ int MyMapWriter::Write()
         bmpFileHeader.bfReserved1 = 0;
         bmpFileHeader.bfReserved2 = 0;
         bmpFileHeader.bfOffBits = 54 + 256*4;
-        fwrite(&bmpFileHeader, sizeof(BITMAPFILEHEADER), 1, pFile);
+        pf.write((const char*)&bmpFileHeader,sizeof(BITMAPFILEHEADER));
 
         bmpInfoHeader.biSize = 40;
         bmpInfoHeader.biWidth =  width;
@@ -158,7 +194,7 @@ int MyMapWriter::Write()
         bmpInfoHeader.biYPelsPerMeter = 0;
         bmpInfoHeader.biClrUsed = 256;
         bmpInfoHeader.biClrImportant = 256;
-        fwrite(&bmpInfoHeader, sizeof( BITMAPINFOHEADER), 1, pFile);
+        pf.write((const char*)&bmpInfoHeader, sizeof( BITMAPINFOHEADER));
 
         cout<<"setting color bands...";
         quad = (RGBQUAD*)malloc(sizeof(RGBQUAD)*256);
@@ -169,7 +205,7 @@ int MyMapWriter::Write()
             quad[i].rgbRed = i;
             quad[i].rgbReserved = 0;
         }
-        fwrite(quad, sizeof(RGBQUAD), 256, pFile);
+        pf.write((const char*)quad, 256*sizeof(RGBQUAD));
         free(quad);
         cout<<"Done."<<endl;
 
@@ -182,14 +218,14 @@ int MyMapWriter::Write()
             for (j=0; j< width; j++)
             {
                 pixVal =  mapData[i* width+j];
-                fwrite(&pixVal, sizeof(unsigned char), 1, pFile);
+                pf.write((const char*)&pixVal, sizeof(unsigned char));
             }
             if (offset!=0)
             {
                 for (j=0; j<offset; j++)
                 {
                     pixVal = 0;
-                    fwrite(&pixVal, sizeof(unsigned char), 1, pFile);
+                    pf.write((const char*)&pixVal, sizeof(unsigned char));
                 }
             }
             for(int tem=0;tem<13;tem++)
@@ -197,7 +233,7 @@ int MyMapWriter::Write()
         }
         cout<<"Done."<<endl;
     }
-    fclose(pFile);
+    pf.close();
     return 0;
 }
 
